c18fcc7059c8cdefdf9223ee1f44f277486e0b99
MZainAsif <kingorootmodapk@gmail.com>
2023-09-01 23:52:45 +0500

    Done

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:shirts_e_commerce/constants.dart';
+import 'package:shirts_e_commerce/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shirts Store',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: bgColor,
+        primarySwatch: Colors.blue,
+        fontFamily: "Gordita",
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
